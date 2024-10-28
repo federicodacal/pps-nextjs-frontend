9665f5942794b7d8fc6b0529ab5395c558a2356d
413831 <alepleins3pp@gmail.com>
2024-10-27 21:13:47 -0300

    add register page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
+import Carousel from '../components/carrousel/Carrousel';
+import CardGrid from '../components/cards/CardGrid';
+import Footer from '../components/footer/Footer';
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center p-24">
-      <span className="text-5xl">HOME</span>
-    </main>
+    <main className="bg-gradient-to-b from-gradientStart to-gradientEnd min-h-screen flex flex-col items-center">
+    <header className="py-6 text-center text-white">
+      <h1 className="text-3xl font-bold">Galería de Audios</h1>
+    </header>
+
+    <section className="w-full max-w-5xl px-4 mt-8">
+      <Carousel />
+    </section>
+
+    <section className="w-full max-w-5xl px-4 mt-8">
+      <CardGrid />
+    </section>
+
+    <Footer />
+  </main>
   );
 }
 
