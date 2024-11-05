0278b097bb31a747990647f47211387b1cec5338
413831 <alepleins3pp@gmail.com>
2024-11-05 20:14:14 -0300

    add basic style to player

--- app/components/audio/AudioCard.tsx
+++ app/components/audio/AudioCard.tsx
@@ -1,68 +1,93 @@
-'use client'
+"use client";
 
-import * as React from 'react'
-const { useMemo, useState, useCallback, useRef } = React
-import { createRoot } from 'react-dom/client'
-import { useWavesurfer } from '@wavesurfer/react'
-import Timeline from 'wavesurfer.js/dist/plugins/timeline.esm.js'
+import * as React from "react";
+const { useMemo, useState, useCallback, useRef } = React;
+import { useWavesurfer } from "@wavesurfer/react";
+import Timeline from "wavesurfer.js/dist/plugins/timeline.esm.js";
+import {
+  BsFillStopFill,
+  BsFillPlayFill,
+  BsSkipForward,
+  BsSkipBackward,
+} from "react-icons/bs";
 
 type AudioProps = {
-    name: string;
-    creator: string;
-    bpm: number;
-    key: string;
-    genre: string;
-    category: string;
-    duration: string;
-    audioUrl: string;
-    onAddToFavorites: () => void;
-    onAddToCart: () => void;
-  };
-  
-  const AudioCard: React.FC<AudioProps> = ({
-    name,
-    creator,
-    bpm,
-    key,
-    genre,
-    category,
-    duration,
-    audioUrl,
-    onAddToFavorites,
-    onAddToCart,
-  }) => {
-    const containerRef = useRef(null)
-    const [urlIndex, setUrlIndex] = useState(0)
-  
-    const { wavesurfer, isPlaying, currentTime } = useWavesurfer({
-      container: containerRef,
-      height: 100,
-      waveColor: 'rgb(200, 0, 200)',
-      progressColor: 'rgb(100, 0, 100)',
-      url: audioUrl,
-      plugins: useMemo(() => [Timeline.create()], []),
-    })
-  
-    
-    const onPlayPause = useCallback(() => {
-      wavesurfer && wavesurfer.playPause()
-    }, [wavesurfer])
-  
-    return (
-      <>
-        <div ref={containerRef} />
-  
-        <p>Current audio: {name}</p>
-  
-  
-        <div style={{ margin: '1em 0', display: 'flex', gap: '1em' }}>
-  
-          <button onClick={onPlayPause} style={{ minWidth: '5em' }}>
-            {isPlaying ? 'Pause' : 'Play'}
-          </button>
+  name: string;
+  creator: string;
+  bpm: number;
+  tone: string;
+  genre: string;
+  category: string;
+  duration: string;
+  audioUrl: string;
+  onAddToFavorites: () => void;
+  onAddToCart: () => void;
+};
+
+const AudioCard: React.FC<AudioProps> = ({
+  name,
+  creator,
+  bpm,
+  tone,
+  genre,
+  category,
+  duration,
+  audioUrl,
+  onAddToFavorites,
+  onAddToCart,
+}) => {
+  const containerRef = useRef(null);
+  const [urlIndex, setUrlIndex] = useState(0);
+
+  const { wavesurfer, isPlaying, currentTime } = useWavesurfer({
+    container: containerRef,
+    height: 100,
+    waveColor: "rgb(200, 0, 200)",
+    progressColor: "rgb(100, 0, 100)",
+    url: audioUrl,
+    plugins: useMemo(() => [Timeline.create()], []),
+  });
+
+  const onPlayPause = useCallback(() => {
+    wavesurfer && wavesurfer.playPause();
+  }, [wavesurfer]);
+
+  return (
+    <>
+      <div className="container">
+        <div className="sub-container">
+          <p>{name}</p>
+          <div ref={containerRef} />
+
+          <div className="wavesurfer-container" />
+
+          <div style={{ margin: "1em 2", display: "flex", gap: "1em" }}>
+            <div>
+              <p>BPM: {bpm}</p>
+
+              <p>Duración : {length}</p>
+
+              <p>Tonalidad: {tone}</p>
+
+              <p>Género: {genre}</p>
+            </div>
+
+            <div>
+              <p>Creador : {creator}</p>
+
+              <p>Categoría : {category}</p>
+            </div>
+
+            <div className="wavesurfer-controls">
+              <button onClick={onPlayPause} style={{ minWidth: "5em" }}>
+                {isPlaying ? <BsFillStopFill /> : <BsFillPlayFill />}
+              </button>
+            </div>
+          </div>
         </div>
-      </>
-    )
-  };
-  
-  export default AudioCard;
+      </div>
+    </>
+  );
+};
+
+export default AudioCard;
