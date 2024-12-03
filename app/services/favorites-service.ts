93dbf683ebfaa775997e00e9336f9f2589e6686a
Federico Dacal <federicodacal@gmail.com>
2024-12-03 11:33:55 -0300

    fix carrousel y url favorites

--- app/services/favorites-service.ts
+++ app/services/favorites-service.ts
@@ -1,17 +1,22 @@
 import axios from 'axios';
-import { NEXT_PUBLIC_PROXY }  from './config';
+import { NEXT_PUBLIC_PROXY, API_BASE_URL }  from './config';
 
 // Favorites Services
 export const addFavorite = async (audio_ID: string, user_ID: string) => {
-    return axios.post(`${NEXT_PUBLIC_PROXY}${audio_ID}`, { user_ID });
+    //return axios.post(`${NEXT_PUBLIC_PROXY}${audio_ID}`, { user_ID });
+    return axios.post(`${API_BASE_URL}/favorites/${audio_ID}`, { user_ID });
   };
   
   export const getFavoritesByUserId = async (user_ID: string) => {
-    return axios.get<any[]>(`${NEXT_PUBLIC_PROXY}${user_ID}`);
+    //return axios.get<any[]>(`${NEXT_PUBLIC_PROXY}${user_ID}`);
+    return axios.get<any[]>(`${API_BASE_URL}/favorites/${user_ID}`);
   };
   
   export const deleteByAudioId = async (audio_ID: string, user_ID: string) => {
-    return axios.delete(`${NEXT_PUBLIC_PROXY}${audio_ID}`, {
+    //return axios.delete(`${NEXT_PUBLIC_PROXY}${audio_ID}`, {
+    //  data: { user_ID },
+    //});
+    return axios.delete(`${API_BASE_URL}/favorites/${audio_ID}`, {
       data: { user_ID },
     });
   };
