a1daddc468862ef50ea2ee37f4341ed21b51a61a
Federico Dacal <federicodacal@gmail.com>
2024-12-08 12:59:40 -0300

    form validations login

--- app/components/login-form/LoginForm.tsx
+++ app/components/login-form/LoginForm.tsx
@@ -4,6 +4,7 @@ import { useState, useTransition } from "react";
 import { useRouter } from "next/navigation";
 import { loginUser } from "@/app/services/auth-service";
 import { useAuth } from "@/app/contexts/AuthContext";
+import Notification from "@/app/components/notification/Notification";
 
 const initUser = () => ({
   email: "",
@@ -15,19 +16,32 @@ export default function LoginForm() {
     const [isPending, startTransition] = useTransition();
     const [userData, setFormData] = useState(initUser());
     const { setAuthToken } = useAuth();
+    const [errors, setErrors] = useState<Record<string, string>>({});
+    const [notification, setNotification] = useState<{ message: string; type: 'error' | 'success' | 'info' } | null>(null);
   
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       const { name, value } = e.target;
       setFormData({ ...userData, [name]: value });
     };
+
+    const validate = () => { 
+      const newErrors: Record<string, string> = {};
+
+      if (!userData.email) newErrors.email = "Ingrese su correo.";
+      if (userData.email && !/\S+@\S+\.\S+/.test(userData.email))
+        newErrors.email = "El correo electrónico no es válido.";
+
+      if (!userData.pwd) newErrors.pwd = "Ingrese su contraseña.";
+
+      setErrors(newErrors);
+      return Object.keys(newErrors).length === 0;
+
+    }
   
     const handleSubmit = async (e: React.FormEvent) => {
       e.preventDefault();
 
-      if(!userData.email || !userData.pwd) {
-        alert("Campos incompletos");
-        return;
-      }
+      if(!validate()) return;
   
       try {
         const response = await loginUser(userData);
@@ -39,6 +53,7 @@ export default function LoginForm() {
           throw new Error(response.message);
         }
       } catch (err: any) {
+        setNotification({ message: `${err?.message || 'Por favor intente nuevamente.'}. Por favor, revise sus credenciales.`, type: 'error' });
         console.log("Error: " + err.message);
       } 
     };
@@ -48,6 +63,7 @@ export default function LoginForm() {
             <form
                 onSubmit={handleSubmit}
                 className="bg-gray-800 p-16 rounded shadow-md w-full mt-1"
+                noValidate
             >
                 <h1 className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</h1>
         
@@ -62,8 +78,8 @@ export default function LoginForm() {
                     value={userData.email}
                     onChange={handleChange}
                     className="mt-1 block w-full bg-gray-700 text-white border border-gray-600 rounded p-2"
-                    required
                 />
+                {errors.email && <p className="text-red-500 font-bold">{errors.email}</p>}
                 </div>
 
                 {/* Password Section */}
@@ -75,8 +91,8 @@ export default function LoginForm() {
                     value={userData.pwd}
                     onChange={handleChange}
                     className="mt-1 block w-full bg-gray-700 text-white border border-gray-600 rounded p-2"
-                    required
                 />
+                {errors.pwd && <p className="text-red-500 font-bold">{errors.pwd}</p>}
                 </div>
 
                 <button
@@ -86,6 +102,9 @@ export default function LoginForm() {
                 >
                 {isPending ? "Procesando..." : "Iniciar sesión"}
                 </button>
+                {notification && (
+                  <Notification message={notification.message} type={notification.type} />
+                )}
             </form>
         </div>
       );
--- app/services/carrousel-service.ts
+++ app/services/carrousel-service.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { NEXT_PUBLIC_DATA_PROXY, DATA_BASE_URL } from './config';
 
+const DEV_URL = 'http://127.0.0.1:5000'
+
 export const getCarrousel = async () => {
-    return axios.get<any[]>(`${NEXT_PUBLIC_DATA_PROXY}/carrousel`);
+    return axios.get<any[]>(`${DEV_URL}/carrousel`);
 };
